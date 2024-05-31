a29d5f156261ea14c019275132414e4a130083c9
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-05-30 22:24:45 -0500

    vista v2

---
+++ src/main/java/pe/edu/idat/app_formularios_mvc/model/PromedioModel.java
@@ -0,0 +1,11 @@
+package pe.edu.idat.app_formularios_mvc.model;
+
+import lombok.Data;
+
+@Data
+public class PromedioModel {
+    private Integer ec1;
+    private Integer ec2;
+    private Integer ec3;
+    private Integer ef;
+}
