8136cd65b7c4d55177b789656dfae2b76875c211
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-05-30 21:43:45 -0500

    initial commit

---
+++ src/main/java/pe/edu/idat/app_formularios_mvc/AppFormulariosMvcApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_formularios_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppFormulariosMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppFormulariosMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/app_formularios_mvc/AppFormulariosMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.app_formularios_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppFormulariosMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
