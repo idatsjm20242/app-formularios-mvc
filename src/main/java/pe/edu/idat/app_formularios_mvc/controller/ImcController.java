88c8ff918078eea33542101baaf8ca02303c96ff
idatsjm20242 <idat.daa2.sjm20242@gmail.com>
2024-06-06 20:10:15 -0500

    definición de la acción del imccontroller

--- src/main/java/pe/edu/idat/app_formularios_mvc/controller/ImcController.java
+++ src/main/java/pe/edu/idat/app_formularios_mvc/controller/ImcController.java
@@ -1,4 +1,27 @@
 package pe.edu.idat.app_formularios_mvc.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.idat.app_formularios_mvc.model.ImcModel;
+
+@Controller
 public class ImcController {
+
+    @GetMapping("/imc")
+    public String imc(Model model){
+        model.addAttribute("mostrar",
+                false);
+        model.addAttribute("objimc",
+                new ImcModel());
+        return "frmimc";
+    }
+
+    @PostMapping("/imc")
+    public String calcularImc(){
+        return "frmimc";
+    }
+
+
 }
